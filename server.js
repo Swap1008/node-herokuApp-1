f49152a231b8bf10db4a74186c3152469d455313
DigvijaySS <suryawanshidigvijay@gmail.com>
2019-05-27 13:08:20 +0530

    HBS with Partials

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express=require('express');
 const app=express();
 const port=process.env.PORT || 3000;
+const hbs=require('hbs');
 
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+// 
 app.get('/',(req,res)=>{
-    res.send("Welcome Index Page");
+    res.render('index.hbs');
 });
 
 app.get('/about',(req,res)=>{
-    res.send("About Us Page");
-})
+    res.render('about.hbs');
+});
 
 
 app.listen(port,()=>{
